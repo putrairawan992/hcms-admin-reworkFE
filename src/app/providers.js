fc00851d1be2f4460b0008e98652c1e4644df77f
TioRafliS <ravenero94@gmail.com>
2024-12-11 01:35:37 +0700

    Fixing login page

--- src/app/providers.js
+++ src/app/providers.js
@@ -2,11 +2,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import SidebarLayout from "./components/sidebarLayout";
 
 export function Providers({ children }) {
   const [isClient, setIsClient] = useState(false);
   const [queryClient] = useState(() => new QueryClient());
+  const pathname = usePathname();
 
   // Set state to true when the component is mounted
   useEffect(() => {
@@ -17,12 +19,19 @@ export function Providers({ children }) {
     return null; // Prevent rendering before the client-side hydration
   }
 
+  // Halaman tanpa sidebar
+  const noSidebarRoutes = ["/login", "/register"];
+
+  const shouldShowSidebar = !noSidebarRoutes.includes(pathname);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
-        <SidebarLayout>
-          {children}
-        </SidebarLayout>
+        {shouldShowSidebar ? (
+          <SidebarLayout>{children}</SidebarLayout>
+        ) : (
+          children
+        )}
       </ChakraProvider>
     </QueryClientProvider>
   );
